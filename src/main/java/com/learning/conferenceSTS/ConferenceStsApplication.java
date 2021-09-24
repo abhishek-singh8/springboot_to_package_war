fafc1a4629be749f075e1653d7f4c7fabf9b54c9
abhishek kumar singh <abhishek.k.singh@boeing.com>
2021-09-24 13:22:46 +0530

    Self_contained jar with integrated spring boot project

---
+++ src/main/java/com/learning/conferenceSTS/ConferenceStsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.conferenceSTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceStsApplication.class, args);
+	}
+
+}
